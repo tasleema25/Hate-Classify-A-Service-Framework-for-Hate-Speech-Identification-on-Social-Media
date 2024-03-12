76b8e2ce3966b2cdf273be32c8a4d5de9082499c
tasleema25 <75987295+tasleema25@users.noreply.github.com>
2024-03-12 20:58:46 +0530

    Add files via upload

---
+++ Remote_User/views.py
@@ -0,0 +1,194 @@
+from django.db.models import Count
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
+import datetime
+import openpyxl
+
+import nltk
+import re
+import string
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from wordcloud import WordCloud, STOPWORDS
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier
+# Create your views here.
+from Remote_User.models import ClientRegister_Model,hate_classify_prediction,detection_ratio,detection_accuracy
+
+def login(request):
+
+
+    if request.method == "POST" and 'submit1' in request.POST:
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            enter = ClientRegister_Model.objects.get(username=username,password=password)
+            request.session["userid"] = enter.id
+
+            return redirect('ViewYourProfile')
+        except:
+            pass
+
+    return render(request,'RUser/login.html')
+
+def Add_DataSet_Details(request):
+
+    return render(request, 'RUser/Add_DataSet_Details.html', {"excel_data": ''})
+
+
+def Register1(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        phoneno = request.POST.get('phoneno')
+        country = request.POST.get('country')
+        state = request.POST.get('state')
+        city = request.POST.get('city')
+        ClientRegister_Model.objects.create(username=username, email=email, password=password, phoneno=phoneno,
+                                            country=country, state=state, city=city)
+
+        return render(request, 'RUser/Register1.html')
+    else:
+        return render(request,'RUser/Register1.html')
+
+def ViewYourProfile(request):
+    userid = request.session['userid']
+    obj = ClientRegister_Model.objects.get(id= userid)
+    return render(request,'RUser/ViewYourProfile.html',{'object':obj})
+
+
+def Predict_TweetClassify_Type(request):
+    if request.method == "POST":
+        Tweet_Message = request.POST.get('Tweet_Message')
+        if request.method == "POST":
+
+            Tweet_Message = request.POST.get('Tweet_Message')
+
+        dataset = pd.read_csv('Tweet_Datasets.csv')
+
+        # Preprocess Data
+        def process_tweet(tweet):
+            return " ".join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])", " ", tweet.lower()).split())
+
+        dataset.rename(columns={'class': 'label', 'tweet': 'review'}, inplace=True)
+
+        def apply_results(label):
+            if (label == 0):
+                return 0  # Hate Speech
+            elif (label == 1):
+                return 1  # Offensive Speech
+            elif (label == 2):
+                return 2  # Non Offensive Speech
+
+        dataset['results'] = dataset['label'].apply(apply_results)
+        dataset.drop(['label'], axis=1, inplace=True)
+        results = dataset['results'].value_counts()
+
+        cv = CountVectorizer()
+
+        dataset["review"] = dataset['review'].apply(process_tweet)
+        x = dataset["review"]
+        y = dataset["results"]
+
+        x = cv.fit_transform(x)
+        print("Tweet")
+        print(x)
+        print("Label")
+        print(y)
+
+        models = []
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+        X_train.shape, X_test.shape, y_train.shape
+
+        print("Naive Bayes")
+
+        from sklearn.naive_bayes import MultinomialNB
+        NB = MultinomialNB()
+        NB.fit(X_train, y_train)
+        predict_nb = NB.predict(X_test)
+        naivebayes = accuracy_score(y_test, predict_nb) * 100
+        print(naivebayes)
+        print(confusion_matrix(y_test, predict_nb))
+        print(classification_report(y_test, predict_nb))
+        models.append(('naive_bayes', NB))
+
+        # SVM Model
+        print("SVM")
+        from sklearn import svm
+        lin_clf = svm.LinearSVC()
+        lin_clf.fit(X_train, y_train)
+        predict_svm = lin_clf.predict(X_test)
+        svm_acc = accuracy_score(y_test, predict_svm) * 100
+        print(svm_acc)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, predict_svm))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, predict_svm))
+        models.append(('svm', lin_clf))
+
+        print("Logistic Regression")
+
+        from sklearn.linear_model import LogisticRegression
+        reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
+        y_pred = reg.predict(X_test)
+        print("ACCURACY")
+        print(accuracy_score(y_test, y_pred) * 100)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, y_pred))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, y_pred))
+        models.append(('logistic', reg))
+
+        print("Decision Tree Classifier")
+        dtc = DecisionTreeClassifier()
+        dtc.fit(X_train, y_train)
+        dtcpredict = dtc.predict(X_test)
+        print("ACCURACY")
+        print(accuracy_score(y_test, dtcpredict) * 100)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, dtcpredict))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, dtcpredict))
+
+        classifier = VotingClassifier(models)
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+
+
+        tweet_data = [Tweet_Message]
+        vector1 = cv.transform(tweet_data).toarray()
+        predict_text = classifier.predict(vector1)
+
+        pred = str(predict_text).replace("[", "")
+        pred1 = pred.replace("]", "")
+
+        prediction = int(pred1)
+
+        if prediction == 0:
+            val = 'Hate Speech'
+        elif prediction == 1:
+            val = 'Offensive Speech'
+        elif prediction == 2:
+            val = 'Non Offensive Speech'
+
+        print(val)
+        print(pred1)
+
+        hate_classify_prediction.objects.create(Tweet_Message=Tweet_Message, Prediction=val)
+
+        return render(request, 'RUser/Predict_TweetClassify_Type.html',{'objs': val})
+    return render(request, 'RUser/Predict_TweetClassify_Type.html')
+
+
+
